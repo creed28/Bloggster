827f12e3dd98ebfb5fddc70d0c16d4e502f28a46
creed669 <pokemonislove21@gmail.com>
2023-05-07 15:30:21 +0300

    fixed auth reg

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from 'mysql'
 export const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: 'root1234',
     database: 'bloggster'
 })
